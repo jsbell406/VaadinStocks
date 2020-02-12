496ace9ffe8009b1430a602b2b61a951711b9064
James Bell <masterpiece0310@hotmail.com>
2020-02-11 21:44:49 -0700

    Genereted project from spring init

---
+++ src/main/java/com/james/vaadinstocks/VaadinstocksApplication.java
@@ -0,0 +1,13 @@
+package com.james.vaadinstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinstocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinstocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/james/vaadinstocks/VaadinstocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.vaadinstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinstocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
